40ec19358a863e4a3257454c05eccc58a70ec78b
Armin <armin.joachimsmeyer@gmail.com>
2018-12-08 04:06:22 +0100

    Version 1.0

--- OpenWindowAlarm.ino
+++ OpenWindowAlarm.ino
@@ -1,13 +1,18 @@
 /*
  * OpenWindowAlarm.cpp
- * Every TEMPERATURE_SAMPLE_SECONDS (24) seconds a sample is taken of the attiny internal temperature sensor which has a resolution of 1 degree.
- * This delay is implemented by n (3) times sleeping at SLEEP_MODE_PWR_DOWN for a period of 8 seconds.
  *
+ * Overview:
+ * Every 24 seconds a sample is taken of the ATtiny internal temperature sensor which has a resolution of 1 degree.
+ * If temperature is lower than "old" temperature value, then an alarm is issued 5 minutes later, if "the condition still holds".
+ * This delay is implemented by 3 times sleeping at SLEEP_MODE_PWR_DOWN for a period of 8 seconds in order to reduce power consumption.
+ *
+ * Detailed description:
  * Open Window is detected after (TEMPERATURE_COMPARE_AMOUNT * TEMPERATURE_SAMPLE_SECONDS) seconds of reading a temperature
  * which value is TEMPERATURE_DELTA_THRESHOLD_DEGREE lower than the temperature (TEMPERATURE_COMPARE_DISTANCE * TEMPERATURE_SAMPLE_SECONDS) seconds before.
+ * It is indicated by a 4 times longer blink every 24 seconds.
  * The internal sensor has therefore 3 minutes time to adjust to the outer temperature, to get even small changes in temperature.
  * The greater the temperature change the earlier the change of sensor value and detection of an open window.
- * After open window detection Alarm is activated after OPEN_WINDOW_MINUTES if actual temperature is not higher than the (minimum temperature + 1) i.e. the window is not closed yet.
+ * After open window detection Alarm is activated after OPEN_WINDOW_MINUTES if actual temperature is not higher than the minimum temperature (+ 1) i.e. the window is not closed yet.
  *
  *  Copyright (C) 2018  Armin Joachimsmeyer
  *  armin.joachimsmeyer@gmail.com
@@ -24,7 +29,7 @@
 
 #include <Arduino.h>
 
-//#define DEBUG
+//#define DEBUG // To see serial output with 115200 baud at pin2
 
 #ifdef DEBUG
 #include "TinySerialOut.h"
@@ -36,7 +41,6 @@
 #include <avr/power.h> // needed for  clock_prescale_set()
 #include <avr/sleep.h> // needed for sleep_enable()
 #include <avr/wdt.h>   // needed for WDTO_8S
-
 
 #define VERSION "1.0"
 
@@ -54,6 +58,7 @@ uint16_t sTemperatureArray[(TEMPERATURE_COMPARE_AMOUNT + TEMPERATURE_COMPARE_DIS
 const uint16_t TEMPERATURE_DELTA_THRESHOLD_DEGREE = 2;
 
 uint16_t sTemperatureMinimumAfterWindowOpen;
+uint16_t sTemperatureAtWindowOpen;
 bool sOpenWindowDetected = false;
 uint8_t sOpenWindowSampleDelayCounter;
 
@@ -78,15 +83,16 @@ uint8_t sOpenWindowSampleDelayCounter;
 #error "LED pin must not be equal TX pin."
 #endif
 
-const uint16_t LED_PULSE_LENGTH = 200; // 500 is well visible 200 is OK
+const uint16_t LED_PULSE_LENGTH = 200; // 500 is well visible, 200 is OK
 
 uint8_t sMCUSRStored; // content of MCUSR register at startup
 
+void PWMtone(uint8_t aPin, unsigned int aFrequency, unsigned long aDurationMillis = 0);
 void signalReading();
-void signal();
+void alarm();
 void initSleep(uint8_t tSleepMode);
 void sleepWithWatchdog(uint8_t aWatchdogPrescaler);
-void sleepDelay();
+void sleepDelay(uint16_t aSecondsToSleep);
 void delayMilliseconds(unsigned int aMillis);
 uint16_t readADCChannelWithReferenceOversample(uint8_t aChannelNumber, uint8_t aReference, uint8_t aOversampleExponent);
 void changeDigisparkClock();
@@ -97,9 +103,11 @@ void changeDigisparkClock();
 
 void setup() {
 
-    // store MCUSR early for later use
+    /*
+     * store MCUSR early for later use
+     */
     sMCUSRStored = MCUSR;
-    MCUSR = 0;
+    MCUSR = 0; // to prepare for next reset or power on
 
 #ifdef DEBUG
     /*
@@ -114,7 +122,7 @@ void setup() {
     pinMode(LED_PIN, OUTPUT);
 
 #ifdef DEBUG
-    writeString(F("START " __FILE__ "\nVersion " VERSION " from " __DATE__ "\nAlarm delay="));
+    writeString(F("START " __FILE__ "\nVersion " VERSION " from " __DATE__ "\nAlarm delay = "));
     writeUnsignedByte(OPEN_WINDOW_ALARM_DELAY_MINUTES);
     writeString(F(" minutes\nMCUSR="));
     writeUnsignedByteHex(sMCUSRStored);
@@ -129,17 +137,15 @@ void setup() {
     write1Start8Data1StopNoParity('\n');
 #endif
 
+    pinMode(TONE_PIN_INVERTED, OUTPUT);
+    pinMode(TONE_PIN, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(ALARM_TEST_PIN, INPUT_PULLUP);
+
     /*
      * init sleep mode
      */
     initSleep(SLEEP_MODE_PWR_DOWN);
-
-    pinMode(TONE_PIN_INVERTED, OUTPUT);
-    pinMode(TONE_PIN, OUTPUT);
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(ALARM_TEST_PIN, INPUT);  // Instead of pinMode(ALARM_TEST_PIN, INPUT_PULLUP);
-    digitalWrite(ALARM_TEST_PIN, 1); // set pullup
-
 // disable Arduino delay() and millis() timer0 and also its interrupts which kills the deep sleep.
     TCCR0B = 0;
 
@@ -147,6 +153,8 @@ void setup() {
      * Initialize ADC channel and reference
      */
     readADCChannelWithReferenceOversample(ADC_TEMPERATURE_CHANNEL_MUX, INTERNAL1V1, 0);
+
+    PWMtone(TONE_PIN, 2200, 100); // signal setup()
 
     /*
      * Blink LED at startup to show OPEN_WINDOW_MINUTES
@@ -160,12 +168,15 @@ void setup() {
         delayMilliseconds(200);
     }
 
+    /*
+     * For testing the speaker connection
+     */
     if (!digitalRead(ALARM_TEST_PIN)) {
 #ifdef DEBUG
         writeString(F("Test signal out"));
         write1Start8Data1StopNoParity('\n');
 #endif
-        signal();
+        alarm();
     }
 
 // disable digital input buffer to save power
@@ -173,11 +184,17 @@ void setup() {
     DIDR0 = (1 << ADC1D) | (1 << ADC2D) | (1 << ADC3D) | (1 << AIN1D) | (1 << AIN0D);
 
     /*
-     * wait 8 seconds, since ATtinys temperature is increased after the digistump boot process
+     * wait 8 seconds, since ATtinys temperature is increased after the micronucleus boot process
      */
     sleepWithWatchdog(WDTO_8S);
 }
 
+/*
+ * Shift temperature history values ad insert new value.
+ * Check if temperature decreases after power on.
+ * Check if window was just opened.
+ * If window was opened check if window still open -> ALARM
+ */
 void loop() {
     uint16_t tTemperatureNewSum = 0;
     uint16_t tTemperatureOldSum = 0;
@@ -192,13 +209,11 @@ void loop() {
         tTemperatureOldSum += sTemperatureArray[i - 1];
         i--;
     }
-
     while (i >= TEMPERATURE_COMPARE_AMOUNT) {
         // shift values to end
         sTemperatureArray[i] = sTemperatureArray[i - 1];
         i--;
     }
-
     while (i > 0) {
         // shift (TEMPERATURE_COMPARE_AMOUNT - 1) values to end and sum them up
         sTemperatureArray[i] = sTemperatureArray[i - 1];
@@ -221,14 +236,13 @@ void loop() {
     && (sTemperatureArray[(sizeof(sTemperatureArray) / sizeof(uint16_t)) - 1] == 0)
             && (sTemperatureArray[(sizeof(sTemperatureArray) / sizeof(uint16_t)) - 2] > 0)
             /*
-             * array is almost full, so check if temperature is lower than at reset time which means,
-             * we ported the sensor from a warm place to its final one
+             * array is almost full, so check if temperature is lower than at power on time which means,
+             * we ported the sensor from a warm place to its final one.
              */
             && (sTemperatureArray[0] < sTemperatureArray[(sizeof(sTemperatureArray) / sizeof(uint16_t)) - 2])) {
         // Start from beginning, clear temperature array
 #ifdef DEBUG
-        writeString(F("Detected porting to a colder place -> reset"));
-        write1Start8Data1StopNoParity('\n');
+        writeString(F("Detected porting to a colder place -> reset\n"));
 #endif
         for (i = 0; i < (sizeof(sTemperatureArray) / sizeof(uint16_t)) - 1; ++i) {
             sTemperatureArray[i] = 0;
@@ -245,33 +259,11 @@ void loop() {
         write1Start8Data1StopNoParity('\n');
 #endif
 
-        if (sOpenWindowDetected) {
-            if (tTemperatureNewSum < sTemperatureMinimumAfterWindowOpen) {
-                // set new minimum
-                sTemperatureMinimumAfterWindowOpen = tTemperatureNewSum;
-            }
-            sOpenWindowSampleDelayCounter++;
-            if (sOpenWindowSampleDelayCounter >= OPEN_WINDOW_SAMPLES) {
-                /*
-                 * After delay, check if already open
-                 */
-                if (tTemperatureNewSum <= (sTemperatureMinimumAfterWindowOpen + TEMPERATURE_COMPARE_AMOUNT)) {
-                    // window still open
-#ifdef DEBUG
-                    writeString(F("Detected window still open -> alarm"));
-                    write1Start8Data1StopNoParity('\n');
-#endif
-                    signal();
-                } else {
-                    // window already closed -> reset values for a new start
-#ifdef DEBUG
-                    writeString(F("Detected window already closed -> start again"));
-                    write1Start8Data1StopNoParity('\n');
-#endif
-                    sOpenWindowDetected = false;
-                }
-            }
-        } else {
+        if (!sOpenWindowDetected) {
+            /*
+             * Check if window just opened
+             */
+            // tTemperatureOldSum can be 0 -> do not use tTemperatureNewSum < tTemperatureOldSum - (TEMPERATURE_DELTA_THRESHOLD_DEGREE * TEMPERATURE_COMPARE_AMOUNT)
             if (tTemperatureNewSum + (TEMPERATURE_DELTA_THRESHOLD_DEGREE * TEMPERATURE_COMPARE_AMOUNT) < tTemperatureOldSum) {
 #ifdef DEBUG
                 writeString(F("Detected window just opened -> check again in "));
@@ -279,13 +271,56 @@ void loop() {
                 writeString(F(" minutes\n"));
 #endif
                 sTemperatureMinimumAfterWindowOpen = tTemperatureNewSum;
+                sTemperatureAtWindowOpen = tTemperatureNewSum;
                 sOpenWindowDetected = true;
                 sOpenWindowSampleDelayCounter = 0;
             }
-        }
-    } // after boot and temperature is decreasing
+        } else {
 
-    sleepDelay();
+            /*
+             * Check if window already closed -> start a new detection
+             */
+            if (tTemperatureNewSum > (sTemperatureMinimumAfterWindowOpen + TEMPERATURE_COMPARE_AMOUNT)) {
+                sOpenWindowDetected = false;
+#ifdef DEBUG
+                writeString(F("Detected window already closed -> start again\n"));
+#endif
+            } else {
+                if (tTemperatureNewSum < sTemperatureMinimumAfterWindowOpen) {
+                    // set new minimum
+                    sTemperatureMinimumAfterWindowOpen = tTemperatureNewSum;
+                }
+
+                /*
+                 * Check for delay
+                 */
+                sOpenWindowSampleDelayCounter++;
+                if (sOpenWindowSampleDelayCounter >= OPEN_WINDOW_SAMPLES) {
+                    /*
+                     * After delay, check if still open - Temperature must be 1 degree lower than temperature at time of open detection
+                     */
+                    if (tTemperatureNewSum <= sTemperatureAtWindowOpen - TEMPERATURE_COMPARE_AMOUNT) {
+                        /*
+                         * Window still open -> ALARM
+                         */
+#ifdef DEBUG
+                        writeString(F("Detected window still open -> alarm"));
+                        write1Start8Data1StopNoParity('\n');
+#endif
+                        alarm();
+                    } else {
+                        // Temperature not 1 degree lower than temperature at time of open detection
+                        sOpenWindowDetected = false;
+#ifdef DEBUG
+                        writeString(F("Detected window maybe not really opened -> start again\n"));
+#endif
+                    }
+                } // delay
+            } // already closed
+        } // !sOpenWindowDetected
+    }  // after power on and temperature is decreasing
+
+    sleepDelay(TEMPERATURE_SAMPLE_SECONDS);
 }
 
 /*
@@ -297,7 +332,7 @@ void changeDigisparkClock() {
     uint8_t tLowFuse = boot_lock_fuse_bits_get(GET_LOW_FUSE_BITS);
     if ((tLowFuse & 0x0F) == 0x01) {
         /*
-         * Here we have High Frequency PLL Clock ( 16 MHz)
+         * Here we have High Frequency PLL Clock (16 or 16.5 MHz)
          */
 #if (F_CPU == 1000000)
         // Divide 16 MHz Pll clock by 16 for Digispark Boards to get the requested 1 MHz
@@ -332,28 +367,41 @@ void changeDigisparkClock() {
     }
 }
 
-void signal() {
-    uint16_t tCounter = 300; // 600 seconds
-    while (tCounter-- != 0) {
+/*
+ * Like tone(), but use OCR1B (PB4) + !OCR1B (PB3)
+ */
+void PWMtone(uint8_t aPin, unsigned int aFrequency, unsigned long aDurationMillis) {
+    tone(aPin, aFrequency / 2, aDurationMillis); // specify half frequency -> PWM doubles it
+    TCCR1 = TCCR1 & 0x0F; // reset mode and disconnect OC1A pins, keep only prescaler
+    GTCCR = (1 << PWM1B) | (1 << COM1B0); // Switch to PWM Mode with OCR1B (PB4) + !OCR1B (PB3) outputs enabled
+    OCR1B = OCR1C / 2; // set PWM to 50%
+}
 
-        // activate LED
-        digitalWrite(LED_PIN, 1);
-        delayMilliseconds(300);
+/*
+ * Generates a 2200 | 1100 Hertz tone signal for 10 minutes and repeat it every hour
+ */
+void alarm() {
+    while (true) {
+        uint16_t tCounter = (600000 / 1300); // 600 seconds / 10 Minutes
+        while (tCounter-- != 0) {
 
-        // deactivate LED
-        digitalWrite(LED_PIN, 0);
+            // activate LED
+            digitalWrite(LED_PIN, 1);
+            delayMilliseconds(300);
 
-        /*
-         * use tone() only for specifying the frequency and then switch to OCR1B PWM mode
-         * in order to use inverted output for increasing the volume
-         */
-        tone(TONE_PIN, 1100); // specify half frequency -> PWM doubles it
-        TCCR1 = TCCR1 & 0x0F; // keep only prescaler
-        GTCCR = (1 << PWM1B) | (1 << COM1B0); // PWM Mode with OCR1B (PB4) + !OCR1B (PB3) outputs enabled
-        OCR1B = OCR1C / 2; // 50% PWM
+            // deactivate LED
+            digitalWrite(LED_PIN, 0);
 
-        delayMilliseconds(1000);
-        tone(TONE_PIN, 550); // specify half frequency -> PWM doubles it
+            PWMtone(TONE_PIN, 2200);  // specify half frequency -> PWM doubles it
+            delayMilliseconds(1000);
+            PWMtone(TONE_PIN, 1100);  // specify half frequency -> PWM doubles it
+        }
+#ifdef DEBUG
+        writeString(F("After 10 minute alarm now pause for 50 minutes"));
+        write1Start8Data1StopNoParity('\n');
+#endif
+        noTone(TONE_PIN);
+        sleepDelay(6000); // Wait 6000 seconds / 50 Minutes
     }
 }
 
@@ -365,7 +413,10 @@ void signalReading() {
     // activate LED
     digitalWrite(LED_PIN, 1);
     if (sOpenWindowDetected) {
-        delayMicroseconds(LED_PULSE_LENGTH * 4);
+        PWMtone(TONE_PIN, 2200); // signal setup()
+        delayMicroseconds(2000); // 2000 can be heard
+        noTone(TONE_PIN);
+        delayMicroseconds(20000); // to let the led light longer
     } else {
         delayMicroseconds(LED_PULSE_LENGTH);
     }
@@ -373,8 +424,9 @@ void signalReading() {
     digitalWrite(LED_PIN, 0);
 }
 
-void sleepDelay() {
-    for (uint8_t i = 0; i < (TEMPERATURE_SAMPLE_SECONDS / 8); ++i) {
+void sleepDelay(uint16_t aSecondsToSleep) {
+    ADCSRA = 0; // disable ADC -> saves 150 - 200 uA
+    for (uint16_t i = 0; i < (aSecondsToSleep / 8); ++i) {
         sleepWithWatchdog(WDTO_8S);
     }
 }
@@ -386,7 +438,6 @@ void delayMilliseconds(unsigned int aMillis) {
 }
 
 #define ADC_PRESCALE8    3 // 104 microseconds per ADC conversion at 1 MHz
-
 uint16_t readADCChannelWithReferenceOversample(uint8_t aChannelNumber, uint8_t aReference, uint8_t aOversampleExponent) {
     uint16_t tSumValue = 0;
     ADMUX = aChannelNumber | (aReference << REFS2);
@@ -402,9 +453,10 @@ uint16_t readADCChannelWithReferenceOversample(uint8_t aChannelNumber, uint8_t a
          */
         loop_until_bit_is_set(ADCSRA, ADIF);
 
-        ADCSRA |= _BV(ADIF); // clear bit to recognize next conversion has finished
+        ADCSRA |= _BV(ADIF); // clear bit to recognize conversion has finished
         // Add value
-        tSumValue += (ADCH << 8) | ADCL;
+        tSumValue += ADCL | (ADCH << 8);
+//        tSumValue += (ADCH << 8) | ADCL; // this does NOT work!
     }
     ADCSRA &= ~_BV(ADATE); // Disable auto-triggering (free running mode)
     return (tSumValue >> aOversampleExponent);
@@ -421,7 +473,6 @@ void initSleep(uint8_t tSleepMode) {
  * WDTO_15MS, 30, 60, 120, 250, WDTO_500MS
  * WDTO_1S to WDTO_8S
  */
-
 void sleepWithWatchdog(uint8_t aWatchdogPrescaler) {
     MCUSR &= ~_BV(WDRF); // Clear WDRF in MCUSR
 
@@ -431,9 +482,16 @@ void sleepWithWatchdog(uint8_t aWatchdogPrescaler) {
 #if defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
 #define WDTCSR  WDTCR
 #endif
-    WDTCSR |= _BV(WDIE) | _BV(WDIF); // Watchdog interrupt enable + reset interrupt flag
+    WDTCSR |= _BV(WDIE) | _BV(WDIF); // Watchdog interrupt enable + reset interrupt flag -> needs ISR(WDT_vect)
     sei();
     sleep_cpu()
     ;
     wdt_disable();
+}
+
+/*
+ * This interrupt wakes up the cpu from sleep
+ */
+ISR(WDT_vect) {
+    ;
 }
