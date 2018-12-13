af7f0b7ff09250526405d0a771c29cf63d69919f
Armin <armin.joachimsmeyer@gmail.com>
2018-12-13 03:50:32 +0100

    Files for bootloader for USB Power Bank operation

---
+++ Burn_upgrade_micronucleus-t85_pullup_at_0.cmd
@@ -0,0 +1,3 @@
+echo Upgrade Digispark Bootloader with micronucleus upload
+%UserProfile%\AppData\Local\Arduino15\packages\digistump\tools\micronucleus\2.0a4\launcher.exe -cdigispark -Uflash:w:upgrade-t85_pullup_at_0.hex:i
+pause
